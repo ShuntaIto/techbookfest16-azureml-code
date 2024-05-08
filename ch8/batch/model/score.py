49fc777c6c166a48d34951256ffef8794d919837
Keita Onabuta <keonabut@microsoft.com>
2024-05-08 16:32:13 +0000

    init

---
+++ ch8/batch/model/score.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import mlflow
+import numpy as np
+import pandas as pd
+
+
+def init():
+    global model
+    model_path = os.path.join(
+        os.environ["AZUREML_MODEL_DIR"],
+        "Walmart_store_sales_model",
+    )
+    model = mlflow.lightgbm.load_model(model_path)
+    logging.info("Init complete")
+
+
+def run(mini_batch):
+    print(f"run method start: {__file__}, run({mini_batch})")
+    results = pd.DataFrame()
+    for input in mini_batch:
+        df_batch = pd.read_csv(input)
+        pd.set_option("display.max_rows", None)
+        print(df_batch.head())
+
+        # Date列からMonth列とDay列を追加し、Date列を削除
+        df_batch["Month"] = pd.to_datetime(df_batch["Date"], format="%d-%m-%Y").dt.month
+        df_batch["Day"] = pd.to_datetime(df_batch["Date"], format="%d-%m-%Y").dt.day
+        df_batch = df_batch.drop(columns="Date")
+
+        # ターゲット変数となる列名を指定
+        col_target = "Weekly_Sales"
+
+        # 学習データと検証データを、特徴量とターゲット変数に分割
+        X_batch = df_batch.drop(columns=col_target)
+        y_batch = df_batch[col_target].to_numpy().ravel()
+
+        print(X_batch.dtypes)
+
+        # 予測
+        pred = model.predict(X_batch)
+
+        # 元データへ予測値とファイルパスを追加
+        df_batch["input"] = input
+        df_batch["pred"] = pred
+
+        results = pd.concat([results, df_batch], ignore_index=True)
+
+    return results
---
+++ ch8/moe/score.py
@@ -0,0 +1,26 @@
+import json
+import logging
+import os
+import mlflow
+import numpy as np
+
+
+# 起動時に呼び出される関数
+def init():
+    global model
+    model_path = os.path.join(
+        os.environ["AZUREML_MODEL_DIR"],
+        "Walmart_store_sales_model",
+    )
+    model = mlflow.lightgbm.load(model_path)
+    logging.info("Init complete")
+
+
+# リクエストを受け取り、推論結果を返す関数
+def run(raw_data):
+    logging.info("model: request received")
+    data = json.loads(raw_data)["data"]
+    data = np.array(data)
+    result = model.predict(data)
+    logging.info("Request processed")
+    return result.tolist()
